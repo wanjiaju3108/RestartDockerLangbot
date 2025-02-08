c2b4edac9886090b951d647b4383a6a4cecd9942
wanjiaju <>
2025-02-08 20:30:40 +0800

    插件开发

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
-
+import requests
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
+@register(name="RestartDockerLangbot", description="通过命令重启langbot的docker", version="0.1", author="魔术柿子")
+class RestartDockerLangbotPlugin(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
@@ -18,28 +18,9 @@ class MyPlugin(BasePlugin):
     @handler(PersonNormalMessageReceived)
     async def person_normal_message_received(self, ctx: EventContext):
         msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
+        if msg == ".重启":
+            url = "http://172.17.0.1:8000/restartLangbot"
+            requests.get(url)
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
